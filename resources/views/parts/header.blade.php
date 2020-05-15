4c98be36447ecfd00b90791391a468fdaf1e02cf
ekusuy26 <y.yuu6221@gmail.com>
2020-05-15 14:17:08 +0900

    Change display depending on whether you are logged in

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -19,6 +19,8 @@
 
 
     <ul class="navbar-nav ml-auto mr-5">
+    <!-- ログインしているかどうかの確認 -->
+    @if (Auth::check())
       <li class="nav-item ml-2">
         <a class="nav-link text-white" href="#">ストック一覧</a>
       </li>
@@ -53,12 +55,14 @@
           </form>
         </div>
       </li>
+      @else
       <li class="nav-item ml-2">
         <a class="nav-link text-white" id="register" href="/register">ユーザ登録</a>
       </li>
       <li class="nav-item ml-2">
         <a class="nav-link text-white" href="#">ログイン</a>
       </li>
+      @endif
     </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
