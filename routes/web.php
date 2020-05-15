698872458fa027092b567dbcdd6e1f455dbcde29
ekusuy26 <y.yuu6221@gmail.com>
2020-05-15 14:32:59 +0900

    Create Article Posting Page

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/drafts/new', 'Auth\PostController@index')->name('drafts.new');
